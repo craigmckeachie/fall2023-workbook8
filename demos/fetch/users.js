0bf87ba53b62aa5fc2c3ede3ae682dc14d9e29ea
Craig McKeachie <craigdmckeachie@gmail.com>
2023-12-11 15:51:44 -0500

    post example

--- demos/fetch/users.js
+++ demos/fetch/users.js
@@ -19,36 +19,18 @@ async function initialize() {
 
 initialize();
 
-
 const newUser = {
-  "name": "Craig Flayer",
-  "username": "cflayer",
-  "email": "cflayer.biz",
-  "address": {
-    "street": "Moxy Trail",
-    "suite": "Suite 280",
-    "city": "Howbout",
-    "zipcode": "58804-1099",
-    "geo": {
-      "lat": "24.8918",
-      "lng": "21.8984"
-    }
-  },
-  "phone": "210.067.6132",
-  "website": "flayergroup.io",
-  "company": {
-    "name": "Flayer Group",
-    "catchPhrase": "Configurable multimedia task-force",
-    "bs": "generate enterprise e-tailers"
-  }
-}
+  name: "Rodney Dangerfield",
+  username: "rdangerfield",
+  email: "rdanger.info",
+};
 
 //POST
-fetch("http://localhost:3000/users", { 
-    method: "POST",
-    headers: "",
-    body:  
-    })
+fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(newUser),
+})
   .then((response) => response.json())
   .then((users) => {
     console.log(users);
