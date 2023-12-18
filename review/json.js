dd5e30a2b8a33ba6080e5bc3acc10c52a8139dbd
Craig McKeachie <craigdmckeachie@gmail.com>
2023-12-18 09:21:30 -0500

    json example

---
+++ review/json.js
@@ -0,0 +1,58 @@
+const christmasSongs = [
+  {
+    title: "Jingle Bells",
+    artist: "James Lord Pierpont",
+    year: 1857,
+  },
+  {
+    title: "Silent Night",
+    artist: "Franz Xaver Gruber",
+    year: 1818,
+  },
+  {
+    title: "Deck the Halls",
+    artist: "Traditional",
+    year: "18th century",
+  },
+  {
+    title: "Feliz Navidad",
+    artist: "José Feliciano",
+    year: 1970,
+  },
+  {
+    title: "Rudolph the Red-Nosed Reindeer",
+    artist: "Johnny Marks",
+    year: 1949,
+  },
+  {
+    title: "White Christmas",
+    artist: "Irving Berlin",
+    year: 1942,
+  },
+  {
+    title: "The Christmas Song",
+    artist: "Mel Tormé and Robert Wells",
+    year: 1945,
+  },
+  {
+    title: "Let It Snow! Let It Snow! Let It Snow!",
+    artist: "Sammy Cahn and Jule Styne",
+    year: 1945,
+  },
+  {
+    title: "Winter Wonderland",
+    artist: "Felix Bernard and Richard B. Smith",
+    year: 1934,
+  },
+  {
+    title: "O Holy Night",
+    artist: "Adolphe Adam",
+    year: 1847,
+  },
+];
+
+const jsonSongs = JSON.stringify(christmasSongs);
+console.log(jsonSongs);
+
+const copyOfChristmasSongs = JSON.parse(jsonSongs);
+console.log(copyOfChristmasSongs);
